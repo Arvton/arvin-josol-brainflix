fdcd431815e5d5158ce0614c7067dda9259f6f72
Arvin Josol <arvin.josol@live.com>
2023-10-17 15:15:29 -0700

    add core partials and test function: - fonts - mixins - style-reset - typography - variables

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
+import './App.scss';
 
-import './App.css';
-
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Test</p>
+      <button>Test button</button>
     </div>
   );
 }
-
-export default App;
