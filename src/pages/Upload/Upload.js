803384d12b743d358748465e5ee629e2f88b42a5
Arvin Josol <arvin.josol@live.com>
2023-10-29 10:45:47 -0700

    create upload page: - replace placeholder elements - re-use existing components - add styles and responsiveness - add missing upload image asset from sprint 2 package

--- src/pages/Upload/Upload.js
+++ src/pages/Upload/Upload.js
@@ -1,32 +1,34 @@
+import uploadPicture from "../../assets/images/Upload-video-preview.jpg"
+import Button from "../../components/Button/Button"
+import Input from "../../components/Input/Input"
 import "./Upload.scss"
 
 export default function Upload() {
     return (
+        <>
+        <div className="upload__divider"></div>
         <section className="upload">
             <h1 className="upload__title">Upload Video</h1>
             <div className="upload__container">
                 {/* create figure component */}
-                <figure className="upload__image">
-                    <img alt=""></img>
-                    <figcaption>Test</figcaption>
+                <figure className="upload__image-container">
+                    <img src={uploadPicture} className="upload__image" alt=""></img>
+                    <figcaption className="upload__label">VIDEO THUMBNAIL</figcaption>
                 </figure>
                 <form className="upload__form">
                     <div className="upload__form-container">
-                        <label className="upload__form-label" htmlFor="input-title">TITLE YOUR VIDEO</label>
-                        <input className="upload__form-input upload__form-input--title" type="text" id="input-title"
-                            name="title" placeholder="Add a title to your video" />
-                        <label className="upload__form-label" htmlFor="input-comment">ADD A VIDEO DESCRIPTION</label>
-                        <textarea className="upload__form-input upload__form-input--description" type="text" id="input-description"
-                            name="description" placeholder="Add a a description to your video"></textarea>
+                        <label className="upload__label" htmlFor="input-title">TITLE YOUR VIDEO</label>
+                        <Input className='upload__form-input' type='text' placeholder='Add a title to your video' />
+                        <label className="upload__label" htmlFor="input-comment">ADD A VIDEO DESCRIPTION</label>
+                        <textarea className='upload__form-textarea' type='text' placeholder='Add a description to your video'></textarea>
                     </div>
-                    <div className="upload__buttons">
-                        {/* add button component, give props */}
-                        <button>PUBLISH</button>
-                        {/* add button component, give props */}
-                        <button>CANCEL</button>
+                    <div className="upload__button-container">
+                        <Button className='upload__button upload__button--publish' buttonContent='PUBLISH' />
+                        <Button className='upload__button upload__button--cancel' buttonContent='CANCEL' />
                     </div>
                 </form>
             </div>
         </section>
+        </>
     )
 }
