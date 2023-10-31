8e6eb97adb3c8e5e2e8035cf1ddb477d4efc61fb
Arvin Josol <arvin.josol@live.com>
2023-10-31 10:45:44 -0700

    get data from mock api: - add axios package - import axios page to video section component - add api data to utils.json - import useEffect for data fetching from mock api - get next videos data from api on page load - set video id to id of first video in next list - fetch main video data when there is a current video id - add render guards

--- src/components/VideoSection/VideoSection.js
+++ src/components/VideoSection/VideoSection.js
@@ -1,37 +1,57 @@
-import mainVideoData from '../../data/video-details.json'
-import nextVideosList from '../../data/videos.json'
+import api from '../../components/utils/utils.json'
 import MainVideo from "../MainVideo/MainVideo";
 import NextVideos from "../NextVideosSection/NextVideosSection";
 import VideoPlayer from "../VideoPlayer/VideoPlayer";
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from "axios";
 import './VideoSection.scss'
 
 export default function VideoSection() {
+    // test getting data and logging it on the console. this is the next videos data
+    const [nextVideoList, setNextVideoList] = useState([])
     // initializes current video id as first video in videos list
-    const [currentVideoId, setCurrentVideoId] = useState(nextVideosList[0].id)
+    const [currentVideoId, setCurrentVideoId] = useState()
     // initializes current video data to be video-info/object with matching id from next video list
-    const [currentVideoData, setCurrentVideoData] = useState(mainVideoData.find((data) => data.id === currentVideoId))
+    const [currentVideoData, setCurrentVideoData] = useState()
+    useEffect(() => {
+        axios.get(`${api.baseUrl}${api.videosEndpoint}${api.apiKey}`)
+            .then(response => {
+                setNextVideoList(response.data)
+                // Set the currentVideoId to the id of the first object in nextVideoList
+                if (response.data.length > 0) {
+                    setCurrentVideoId(response.data[0].id);
+                }
+            })
+    }, [])
+
+    useEffect(() => {
+        // Update currentVideoData when currentVideoId changes
+        if (currentVideoId) {
+            axios.get(`${api.baseUrl}${api.videosEndpoint}/${currentVideoId}${api.apiKey}`)
+                .then(response => {
+                    setCurrentVideoData(response.data)
+                })
+        }
+    }, [currentVideoId]);
 
     const changeCurrentVideoId = (id => {
         setCurrentVideoId(id)
-        const newVideoData = mainVideoData.find((data) => data.id === id)
-        setCurrentVideoData(newVideoData)
     })
 
     return (
         <main>
             <section className="videos">
-                <VideoPlayer thumbnail={currentVideoData.image} />
+                {currentVideoData && <VideoPlayer thumbnail={currentVideoData.image} />}
                 <div className="videos__container">
                     <div className="videos__current">
-                        <MainVideo currentVideoData={currentVideoData} />
+                        {currentVideoData && <MainVideo currentVideoData={currentVideoData} />}
                     </div>
                     <div className="videos__next">
-                        <NextVideos
-                            nextVideosList={nextVideosList}
+                        {nextVideoList && <NextVideos
+                            nextVideosList={nextVideoList}
                             changeCurrentVideoId={changeCurrentVideoId}
                             currentVideoId={currentVideoId}
-                        />
+                        />}
                     </div>
                 </div>
             </section>
